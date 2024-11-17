ed1026119e873a953d9f02ff47c7cbc0dabeb209
azmick <azmi.karakaya@ogr.sakarya.edu.tr>
2024-11-17 13:51:01 +0300

    İlk blog projemi ekledim

---
+++ content/blog/blog-deneme/index.md
@@ -0,0 +1,8 @@
+---
+title: "Yeni Blog Yazısı"
+date: "2024-11-17"
+description: "Bu yeni bir blog yazısıdır."
+---
+
+## Hoş Geldiniz!
+Bu benim yeni blog yazım. Burada içerik oluşturmayı öğreniyorum!
--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Yazılımcı Günlüğüm`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Azmi Çağrı Karakaya`,
+      summary: `Yazılım dünyasında öğrendiklerimi anlatarak aktarmaya çalışıyorum.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `IchCugrii`,
     },
   },
   plugins: [
